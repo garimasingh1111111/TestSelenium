71252ad3f8b0a82352b32390271d8a2b5bc3d588
Garima Singh <garimasingh1111111@gmail.com>
2022-12-14 12:10:36 +0530

    feat(): add register and login page

---
+++ src/Login.java
@@ -0,0 +1,11 @@
+public class Login{
+    static String myMethod() {
+       // System.out.println("Hello World!");
+        return "Hello World!";
+      }
+
+      public static void main(String[] args) {
+        System.out.println(Login.myMethod());
+      }
+
+}
